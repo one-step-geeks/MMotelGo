a3120d562a69067bafd6f92b558876e9d89e167e
tangyefei <tyfccsu@gmail.com>
2022-09-24 11:56:26 +0800

    feat: 消费项和收付款的交互优化

--- src/pages/pms/Order/components/PayDrawer/index.tsx
+++ src/pages/pms/Order/components/PayDrawer/index.tsx
@@ -8,12 +8,32 @@ import {
   ProFormDateTimePicker,
   ProFormTextArea,
   ProFormSelect,
+  ProFormRadio,
 } from '@ant-design/pro-components';
 import { message, Tabs, Input, Space, Form, Checkbox, Button } from 'antd';
 import { useEffect, useState } from 'react';
 import './style.less';
 
 interface FormOrder {}
+
+export const payOrRefundOptions = [
+  {
+    label: '收款',
+    value: 1,
+  },
+  {
+    label: '收押金',
+    value: 2,
+  },
+  {
+    label: '退款',
+    value: 3,
+  },
+  {
+    label: '退押金',
+    value: 4,
+  },
+];
 
 export function usePayOrRefundDrawer(onSuccess: () => void) {
   const [form] = Form.useForm<FormOrder>();
@@ -26,10 +46,17 @@ export function usePayOrRefundDrawer(onSuccess: () => void) {
       form={form}
       layout="horizontal"
       grid
+      labelCol={{
+        md: 4,
+      }}
       autoFocusFirstInput
       visible={visible}
+      initialValues={{
+        type: 1,
+      }}
       drawerProps={{
         width: 540,
+
         closeIcon: (
           <>
             <Space>
@@ -48,20 +75,11 @@ export function usePayOrRefundDrawer(onSuccess: () => void) {
       submitTimeout={2000}
       onFinish={async (values) => {
         try {
-          // if (notice?.id) {
-          //   await services.OrderController.updatePay({
-          //     ...values,
-          //     id: notice?.id,
-          //   });`
-          //   message.success('修改成功');
-          // } else {
           await services.OrderController.addPayOrRefund({
-            type: payType === 'pay' ? 1 : 3,
             orderId,
             ...values,
           });
           message.success('添加成功');
-          // }
           setVisible(false);
           form.resetFields();
           onSuccess();
@@ -73,14 +91,26 @@ export function usePayOrRefundDrawer(onSuccess: () => void) {
         defaultActiveKey={payType}
         onChange={(value) => {
           setPayType(value);
+          form.setFieldsValue({
+            type: value === 'pay' ? 1 : 3,
+          });
         }}
       >
         <Tabs.TabPane tab="收款" key="pay" />
         <Tabs.TabPane tab="退款" key="refund" />
       </Tabs>
 
+      <ProFormRadio.Group
+        name="type"
+        label="项目"
+        options={
+          payType === 'pay'
+            ? payOrRefundOptions.slice(0, 2)
+            : payOrRefundOptions.slice(2)
+        }
+      />
+
       <ProFormSelect
-        colProps={{ md: 12 }}
         label="支付方式"
         rules={[{ required: true, message: '请选择支付方式' }]}
         name="paymentTypeId"
