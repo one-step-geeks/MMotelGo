60b7247761edc6fe1ca39d89bcae2fd4ecd01fe1
wy187819 <wy187819@alibaba-inc.com>
2022-09-25 15:31:29 +0800

    feat: 权限控制

--- src/access.ts
+++ src/access.ts
@@ -4,12 +4,28 @@ import type { MenuDataItem } from '@ant-design/pro-layout';
  */
 export default (initialState: SYSTEM.InitialState) => {
   const { menuAuthorityList, overAllAuthorityList } = initialState || {};
+
+  // 所有主菜单
   const mainMenuList = menuAuthorityList?.reduce((all, module) => {
     return [...all, ...module.mainMenuList];
   }, [] as ACCOUNT.MainMenu[]);
+
+  // 所有子菜单
+  const subMenuList = mainMenuList?.reduce((all, main) => {
+    return [...all, ...main.subMenuList];
+  }, [] as ACCOUNT.SubMenu[]);
+
   return {
     canSeeRouter: (route: MenuDataItem) => {
-      return mainMenuList?.find((main) => main.mainMenuId === route.mainMenuId);
+      return !!mainMenuList?.find(
+        (main) => main.mainMenuId === route.mainMenuId,
+      );
+    },
+    canSeeSubRouter: (route: MenuDataItem) => {
+      return !!subMenuList?.find((sub) => sub.menuId === route.subMenuId);
+    },
+    canSeeSubMenu: (menuId: number) => {
+      return !!subMenuList?.find((sub) => sub.menuId === menuId);
     },
   };
 };
