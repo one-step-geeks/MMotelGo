3e39d15199ff00624e37855f8b2c9fab1f9cb4b9
tangyefei <tyfccsu@gmail.com>
2022-10-22 21:57:18 +0800

    fix: #1152914342001000099 连续英文字符没断行的问题

--- src/pages/pms/Order/components/OrderDetailDrawer/index.tsx
+++ src/pages/pms/Order/components/OrderDetailDrawer/index.tsx
@@ -676,8 +676,8 @@ export function useOrderDetailDrawer(
             </ProCard>
             <ProCard>
               <div className="custom-form-item">
-                <label>备注：</label>
-                {data?.order.remark}
+                <label>备注：?</label>
+                <div>{data?.order.remark}</div>
               </div>
               <div className="custom-form-item">
                 <label>订单编号：</label>
