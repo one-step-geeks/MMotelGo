8818db510b4fa04e7964579c2cda5409a23ee261
ytyjxym <ytyjxym@qq.com>
2023-01-07 20:52:53 +0800

    feat: 组件抽象

--- src/pages/pms/Statistic/PaymentManage/components/PaymentRecord/component/ExportModalForm/index.tsx
+++ src/pages/pms/Statistic/PaymentManage/components/PaymentRecord/component/ExportModalForm/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useIntl } from 'umi';
 import { Alert, Button } from 'antd';
 import { DrawerForm } from '@ant-design/pro-form';
+import { PaymentRecordContext } from '../../context';
 
 const PaymentRecordExportModalForm: React.FC = () => {
   const intl = useIntl();
-
+  const { drawerFormRef } = useContext(PaymentRecordContext);
   return (
     <DrawerForm
+      formRef={drawerFormRef}
       trigger={
         <Button type="primary">{intl.formatMessage({ id: '报表导出' })}</Button>
       }
