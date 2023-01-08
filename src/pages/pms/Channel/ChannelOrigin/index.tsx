4f9bb8dd8a8959bee8fa2d7905de3aa52a98c8e2
ytyjxym <ytyjxym@qq.com>
2023-01-08 11:52:23 +0800

    feat: 渠道调整

--- src/pages/pms/Channel/ChannelOrigin/index.tsx
+++ src/pages/pms/Channel/ChannelOrigin/index.tsx
@@ -7,26 +7,52 @@ const TradeManage: React.FC = () => {
   const columns = useMemo<ProColumns[]>(() => {
     return [
       {
-        title: intl.formatMessage({ id: '序号' }),
-      },
-      {
-        title: intl.formatMessage({ id: '渠道来源' }),
+        title: intl.formatMessage({ id: '渠道订单号' }),
+        hideInTable: true,
       },
       {
         title: intl.formatMessage({ id: '获取时间' }),
+        hideInTable: true,
+        valueType: 'dateRange',
+        dataIndex: 'date',
+      },
+      {
+        title: intl.formatMessage({ id: '渠道来源' }),
+        hideInTable: true,
+        valueType: 'select',
+      },
+      {
+        title: intl.formatMessage({ id: '邮箱' }),
+        hideInTable: true,
+        valueType: 'select',
+      },
+      {
+        title: intl.formatMessage({ id: '序号' }),
+        search: false,
+      },
+      {
+        title: intl.formatMessage({ id: '渠道来源' }),
+        search: false,
+      },
+      {
+        title: intl.formatMessage({ id: '获取时间' }),
+        search: false,
       },
       {
         title: intl.formatMessage({ id: '获取邮箱' }),
+        search: false,
       },
       {
         title: intl.formatMessage({ id: '渠道订单号' }),
+        search: false,
       },
       {
         title: intl.formatMessage({ id: '邮件标题' }),
+        search: false,
       },
     ];
   }, []);
-  return <ProTable columns={columns} />;
+  return <ProTable scroll={{ x: 'max-content' }} columns={columns} />;
 };
 
 export default TradeManage;
