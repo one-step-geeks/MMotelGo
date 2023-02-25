255a02f6771675d3e3423f110e78bed9df271266
xiong <yueminxiong.xym@alibaba-inc.com>
2023-02-25 12:22:39 +0800

    feat: 状态添加

--- src/pages/pms/RoomState/components/OrderDrawer/OrderBox.tsx
+++ src/pages/pms/RoomState/components/OrderDrawer/OrderBox.tsx
@@ -1,5 +1,6 @@
 import { Typography } from 'antd';
 import React, { useState } from 'react';
+import classnames from 'classnames';
 import './style.less';
 
 const Text = Typography.Text;
@@ -11,12 +12,13 @@ interface Props {
 }
 
 const OrderBox: React.FC<Props> = (props) => {
-  const { order, onOrder } = props;
-
-  let className = 'room-order-box status-1';
-
+  const { order, record, onOrder } = props;
+  console.log(record);
   return (
-    <div className={className} onClick={onOrder}>
+    <div
+      className={classnames('room-order-box', `status-${1}`)}
+      onClick={onOrder}
+    >
       <Text>{order?.reserveName || 'none'}</Text>
       <Text className="orgin-source">{order?.channelTypeName || '自来客'}</Text>
     </div>
